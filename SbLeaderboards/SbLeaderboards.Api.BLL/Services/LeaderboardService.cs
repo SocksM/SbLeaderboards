e12d257afad350c9a42fe89e9b4a79fd937d2fa4
SocksM <67207504+SocksM@users.noreply.github.com>
2024-09-23 22:03:52 +0200

    womp

--- SbLeaderboards/SbLeaderboards.Api.BLL/Services/LeaderboardService.cs
+++ SbLeaderboards/SbLeaderboards.Api.BLL/Services/LeaderboardService.cs
@@ -16,10 +16,10 @@ namespace SbLeaderboards.Api.BLL.Services
 
 
 
-		public List<Stats> GetLeaderboard(bool allowMultipleProfiles = false, StatType sort = StatType.SkyblockExp)
+		public List<Stats> GetLeaderboard(StatType sort = StatType.SkyblockExp)
 		{
-			// Fetch all stats with their associated profiles and players
-			var allStats = _statsService.GetWhere(s => true); // No condition, get all stats
+			var leaderboards = new List<Stats>();
+			var allStats = _statsService.GetAll();
 
 			// Join with profiles to get PlayerId from Profile
 			var statsWithProfiles = allStats.Join(
@@ -28,29 +28,6 @@ namespace SbLeaderboards.Api.BLL.Services
 				profile => profile.Id,
 				(stats, profile) => new { stats, profile.PlayerId }
 			).ToList();
-
-			// If multiple profiles are not allowed, group by PlayerId and select the best profile for each player
-			if (!allowMultipleProfiles)
-			{
-				statsWithProfiles = statsWithProfiles
-					.GroupBy(sp => sp.PlayerId)
-					.Select(g => g.OrderByDescending(sp => GetStatValueByType(sp.stats, sort)).First())
-					.ToList();
-			}
-
-			// Extract the stats for sorting
-			var leaderboard = statsWithProfiles.Select(sp => sp.stats).ToList();
-
-			// Sorting based on the selected StatType
-			leaderboard = sort switch
-			{
-				StatType.SkyblockExp => leaderboard.OrderByDescending(s => s.SkyblockExp).ToList(),
-				StatType.MiningExp => leaderboard.OrderByDescending(s => s.MiningExp).ToList(),
-				StatType.FarmingExp => leaderboard.OrderByDescending(s => s.FarmingExp).ToList(),
-				StatType.CombatExp => leaderboard.OrderByDescending(s => s.CombatExp).ToList(),
-				// Add other cases for other StatTypes as needed
-				_ => leaderboard.OrderByDescending(s => s.SkyblockExp).ToList(), // Default sorting
-			};
 
 			return leaderboard;
 		}

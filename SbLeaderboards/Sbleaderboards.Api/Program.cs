e870deb2f47f9e57fc17c07271bcc9f7244a8177
SocksM <67207504+SocksM@users.noreply.github.com>
2024-11-22 21:25:54 +0100

    test

--- SbLeaderboards/Sbleaderboards.Api/Program.cs
+++ SbLeaderboards/Sbleaderboards.Api/Program.cs
@@ -10,7 +10,7 @@ namespace SbLeaderboards.Api
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
-
+			//
 			// Add services to the container.
 			builder.Services.AddControllers();
 			builder.Services.AddEndpointsApiExplorer();
